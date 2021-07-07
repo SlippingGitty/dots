40c7aa9d4eea60aa47e1d1b7dfc79e7e41165286
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-07-07 02:11:09 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ clear
                 git clone https://aur.archlinux.org/yay.git
                 cd yay
                 makepkg -si
-                yay -S discord-canary st curl npm flameshot qbittorrent firefox rofi lxapperance thunar tumbler alacritty krita neovim git zsh neofetch
+                yay -S discord-canary st curl npm flameshot qbittorrent firefox rofi lxappearance thunar tumbler alacritty krita neovim git zsh neofetch
                 echo Finished.
                 sleep 3
             # Installs the Arch packages I want
